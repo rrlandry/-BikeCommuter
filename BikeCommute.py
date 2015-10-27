477ca3897881859ca646d72b8b3c62eaf6dc2b76
rrlandry <ryanlandrydev@gmail.com>
2015-10-27 15:06:40 -0700

    correctly increments the current commutes

--- BikeCommute.py
+++ BikeCommute.py
@@ -1,16 +1,23 @@
-import gspread
 import json
+import gspread
 from oauth2client.client import SignedJwtAssertionCredentials
 
+# Grab credentials and authorize gspread
 json_key = json.load(open('cert.json'))
 scope = ['https://spreadsheets.google.com/feeds']
-
 credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
-
 gc = gspread.authorize(credentials)
 
-# Open a worksheet from spreadsheet with one shot
-wks = gc.open("TestSheet").sheet1
+# Get the upgrades sheet from the "Bike Commuting" spreadsheet.
+upgrades = gc.open_by_key("1DbbcRTwytdVD9khKGJmea5R1GdH41-4vfiq1I-UHxvs").worksheet("Upgrades")
 
-cell = wks.acell('B2')
-wks.update_acell('B2', int(cell.value)+1)
+# Get current commutes cell
+current_commutes = upgrades.acell('C2')
+
+# Increment the current commutes cell
+new_commutes_completed = int(current_commutes.value)+1
+upgrades.update_acell('C2', new_commutes_completed) 
+
+# Dump to the screen
+print "Current Commutes Completed:", new_commutes_completed
+
